8cd4af3cd135b158fb64394312c7ff5a968b3548
CSI-Francisco-Ruiz <24-087@sanignacio.pr>
2022-02-14 12:00:39 -0400

    dsf

--- Projects/Hangman/Hangman.py
+++ Projects/Hangman/Hangman.py
@@ -1,3 +1,22 @@
+import json, ssl
+import random
+import urllib.request
+from RandomCryptoCoin import RandomCryptoCoin
+
+def getWord():
+
+    cryptocoinLink = "https://random-data-api.com/api/crypto_coin/random_crypto_coin"
+
+    request = urllib.request.Request(cryptocoinLink)
+
+    requestWord= json.loads(urllib.request.linkopen(request).open())
+
+    current_coin =RandomCryptoCoin(**requestWord)
+    
+    return current_coin.coin_name
+
+
+
 Steps = ["""
         |____________|
         |            |
@@ -65,4 +84,5 @@ Steps = ["""
         """
 ]
 
-print(Steps[0])
+print(Steps[0]) 
+
