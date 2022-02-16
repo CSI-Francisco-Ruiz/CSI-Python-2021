8bf5dbf6a3bc91c1700efdd95564c54aa5bf919b
CSI-Francisco-Ruiz <24-087@sanignacio.pr>
2022-02-16 11:37:59 -0400

    Update Hangman.py

--- Projects/Hangman/Hangman.py
+++ Projects/Hangman/Hangman.py
@@ -3,6 +3,8 @@ import random
 import urllib.request
 from RandomCryptoCoin import RandomCryptoCoin
 
+ssl._create_default_https_context = ssl._create_unverified_context
+
 def getWord():
 
     cryptocoinLink = "https://random-data-api.com/api/crypto_coin/random_crypto_coin"
@@ -11,11 +13,9 @@ def getWord():
 
     requestWord= json.loads(urllib.request.linkopen(request).open())
 
-    current_coin =RandomCryptoCoin(**requestWord)
+    present_coin =RandomCryptoCoin(**requestWord)
     
-    return current_coin.coin_name
-
-
+    return present_coin.coin_name
 
 Steps = ["""
         |____________|
@@ -85,4 +85,7 @@ Steps = ["""
 ]
 
 print(Steps[0]) 
+print("Type a letter of your choice, be wary, as a wrong letter will cost you")
 
+Incorrect_integers = ["0","1","2","3","4","5","6","7","8","9"]
+Incorrect_characters = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+","`","~","[","{","]","}","\\","|",";",":",",","<",".",">","/","?"]
