2fb7b4bb8befda70e831b7f6e13e80de5774eb90
CSI-Francisco-Ruiz <24-087@sanignacio.pr>
2022-03-30 11:21:54 -0400

    Update Selenium.py

--- Projects/Web-Scraping/Selenium.py
+++ Projects/Web-Scraping/Selenium.py
@@ -1,11 +1,13 @@
-import selenium
-from selenium import webdriver
-from pathlib import Path
 import os
+from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.options
 
 myOutputPath = Path(__file__).parents[0]
 geckodriverPath = f"(myPath)/geckodriver.exe"
 driver = webdriver.Chrome()
 driver.get("https://www.google.com/")
 driver.maximize_window()
-driver.find_element(By.NAME, 'q').send_keys('Bauer')
+driver.find_element(By.NAME, 'q').send_keys('Discord mod')
