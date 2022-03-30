01381c14d1c54ce4fe9bfdaee7d4cdce5e84b53c
CSI-Francisco-Ruiz <24-087@sanignacio.pr>
2022-03-30 11:24:11 -0400

    Update Selenium.py

--- Projects/Web-Scraping/Selenium.py
+++ Projects/Web-Scraping/Selenium.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.options
+from selenium.webdriver.common.options import Options
 
 myOutputPath = Path(__file__).parents[0]
 geckodriverPath = f"(myPath)/geckodriver.exe"
