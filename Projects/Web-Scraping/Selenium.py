bd5f3c1cd6ad9df7d1406d15c5396834ff680fd3
CSI-Francisco-Ruiz <24-087@sanignacio.pr>
2022-04-21 11:42:41 -0400

    sedf

--- Projects/Web-Scraping/Selenium.py
+++ Projects/Web-Scraping/Selenium.py
@@ -1,13 +1,25 @@
+from fileinput import close
 import os
 from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.options import Options
+from selenium.webdriver.chrome.options import Options 
 
-myOutputPath = Path(__file__).parents[0]
-geckodriverPath = f"(myPath)/geckodriver.exe"
-driver = webdriver.Chrome()
-driver.get("https://www.google.com/")
+
+myPath = Path(__file__).parents[0]
+driverPath = f"{myPath}/chromedriver.exe"
+
+options = Options()
+# options.headless = True
+
+driver = webdriver.Chrome(executable_path= driverPath, options = options )
+
+
+driver.get("https://www.ebay.com/")
 driver.maximize_window()
-driver.find_element(By.NAME, 'q').send_keys('Discord mod')
+driver.switch_to.parent_frame()
+searchbar = driver.find_element(By.ID, 'gh-ac-box')
+searchbar.send_keys("whatever")
+#print(price)
+#driver.close()
